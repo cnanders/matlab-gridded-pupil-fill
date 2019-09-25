20ea9edce277a98bc3b0b6d8f33ad83e7728f5ed
Chris Anderson <cnanders@gmail.com>
2019-09-25 11:11:03 -0700

    updating asml hex'

--- tests/test_getHexapoleAsml.m
+++ tests/test_getHexapoleAsml.m
@@ -7,17 +7,25 @@ addpath(cPath);
 import griddedPupilFill.*
 
 
-[xOut, yOut, iOut] = getHexapoleAsml(0.8, 30, 80);
+% [xOut, yOut, iOut] = getHexapoleAsml();
+
+[xOut, yOut, iOut] = getHexapoleAsml(...
+    'sigmaOfCenterRing', 0.33/0.5 * 1, ...
+    'sigmaOfCenterOfOuterRings', 0.33 / 0.5 * 1.17, ...
+    'sigmaOfOuterRings', 0.33 / 0.5 * 0.75 ...
+);
+    
 
 figure
-subplot(131)
-plot(xOut, '.-')
+hAxis1 = subplot(131);
+plot(hAxis1, xOut, '.-')
 ylim([-1 1])
-subplot(132)
-plot(yOut, '.-')
+hAxis2 = subplot(132);
+plot(hAxis2, yOut, '.-')
 ylim([-1 1])
-subplot(133)
-plot(iOut, '.-')
+hAxis3 = subplot(133);
+plot(hAxis3, iOut, '.-')
+axis(hAxis3, 'image');
 
 
 figure
@@ -30,17 +38,19 @@ ylim([-1 1])
 [x, y, t] = getTimeSignals(xOut, yOut, iOut, 24e-6, 100e-3);
 
 figure
-subplot(131)
-plot(t, x, '.-')
-ylim([-1 1])
-subplot(132)
-plot(t, y, '.-')
-ylim([-1 1])
-subplot(133)
-plot(x, y, 'o-')
-xlim([-1 1])
-ylim([-1 1])
+hAxis1 = subplot(131);
+plot(hAxis1, t, x, '.-')
+ylim(hAxis1, [-1 1])
 
+hAxis2 = subplot(132);
+plot(hAxis2, t, y, '.-')
+ylim(hAxis2, [-1 1])
+
+hAxis3 = subplot(133);
+plot(hAxis3, x, y, 'o-')
+axis(hAxis3, 'image')
+xlim(hAxis3, [-1 1])
+ylim(hAxis3, [-1 1])
 
 
 
